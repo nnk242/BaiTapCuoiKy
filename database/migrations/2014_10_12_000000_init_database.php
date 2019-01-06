0a49a77818261201b1bf3701d280d5f04cf80e28
nnk242 <khangnn@appota.com>
2019-01-06 18:45:54 +0700

    init database #12

--- database/migrations/2014_10_12_000000_init_database.php
+++ database/migrations/2014_10_12_000000_init_database.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class InitDatabase extends Migration
 {
     /**
      * Run the migrations.
@@ -17,10 +17,11 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'member', 'staff'])->default('member');
             $table->rememberToken();
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
